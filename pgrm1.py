143904015fb38fd87fc37c2fd841c0c8c5e6a7c9
Ritik-Katiyar <katiyarritik056@gmail.com>
2024-10-14 01:54:43 +0530

    first commit

---
+++ pgrm1.py
@@ -0,0 +1,21 @@
+
+arr = [1,3,5,2,7,6,4,5,8,10,12,4,5,2]
+x = set(arr)
+print(arr)
+print(x)
+print(len(arr))
+print(len(x))
+
+print(sorted(arr))
+dict={
+'name':"Ritik",2:"age",3:"Rollno"
+}
+print(dict.items())
+for item in dict.items():
+    print(item)
+for i in dict:
+    print(i)
+  
+
+
+     
